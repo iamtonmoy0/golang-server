8ada7a0469672f916d9b0053baea0cd06e1dd3ae
iamtonmoy0 <tonmoy2code@gmail.com>
2023-11-17 16:32:24 -0500

    post method added

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	
+	"strings"
 )
 
 func main() {
 	fmt.Println("server started")
-	getReq()
+	// getReq()
+	postReq()
 }
 
 func getReq() {
@@ -23,6 +24,29 @@ func getReq() {
 	// Get the body of the request
 	fmt.Println("the status code is", response.StatusCode)
 	fmt.Println("the length is", response.ContentLength)
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(content))
+}
+func postReq() {
+	myurl := "https://google.com"
+	// json
+	requestBody := strings.NewReader(`
+	{
+		"name": "John Doe",
+		"age": 35,
+		"platform":"google.com"
+
+	}
+	`)
+	response, err := http.Post(myurl, "application/json", requestBody)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	// Get the body of the request
 	content, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		panic(err)

8c7ff962bd982311a06ba6ca6c0c53da38313d2e
iamtonmoy0 <tonmoy2code@gmail.com>
2023-11-18 17:08:02 -0500

    gorilla mux added

--- main.go
+++ main.go
@@ -2,66 +2,21 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
-	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("server started")
-	// getReq()
-	// postReq()
-	getJson()
+	r := mux.NewRouter()
+	r.HandleFunc("/",home)
+	
+	log.Fatal(http.ListenAndServe(":8000",r))
 }
 
-func getReq() {
-	url := "https://google.com"
-	response, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>hello world</h1>"))
 
-	defer response.Body.Close()
-	// Get the body of the request
-	fmt.Println("the status code is", response.StatusCode)
-	fmt.Println("the length is", response.ContentLength)
-	content, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(content))
-}
-func postReq() {
-	myurl := "https://google.com"
-	// json
-	requestBody := strings.NewReader(`
-	{
-		"name": "John Doe",
-		"age": 35,
-		"platform":"google.com"
-
-	}
-	`)
-	response, err := http.Post(myurl, "application/json", requestBody)
-	if err != nil {
-		panic(err)
-	}
-	defer response.Body.Close()
-	// Get the body of the request
-	content, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(content))
-}
-
-func getJson() {
-	url := "https://jsonplaceholder.typicode.com/posts"
-	res, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer res.Body.Close()
-	bodyBytes, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(bodyBytes))
 }

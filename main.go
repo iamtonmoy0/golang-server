f3ca2cd22d58723daeb2c5f83a4367c41a35ee59
iamtonmoy0 <tonmoy2code@gmail.com>
2023-11-20 14:48:45 -0500

    get single course by id added

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 // models
-type Courses struct {
+type Course struct {
 	ID     int     `json:"id"`
 	Name   string  `json:"name"`
 	Price  int     `json:"price"`
@@ -27,23 +28,41 @@ var courses []Course
 func main() {
 	fmt.Println("server started")
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/courses", getCourses)
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("POST")
+	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
+// home func
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>hello world</h1>"))
 
 }
 
 // get all courses
-func getCourses(w http.ResponseWriter, r *http.Request) {
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
 }
 
 // get courses by id
-func getCourse(w http.ResponseWriter, r *http.Request) {
-
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		// Handle the error (e.g., return a bad request response)
+		http.Error(w, "Invalid ID parameter", http.StatusBadRequest)
+		return
+	}
+	for _, course := range courses {
+		if course.ID == id {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No course found ")
+	return
 }
+// create course
